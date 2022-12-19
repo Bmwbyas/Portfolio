65db280686af20e7b523fef2cc71b46011943e60
Askerko Alexandr <askerko50@gmail.com>
2022-12-19 21:23:26 +0300

    feat(contacts )

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -3,6 +3,7 @@ import s from './Contacts.module.scss'
 import {Button} from "../../common/components/Button/Button";
 import {Title} from "../../common/components/title/Title";
 import {TitleDesc} from "../../common/components/TitleDesc/TitleDesc";
+import {myContactsIcon} from "./myContacts";
 
 
 export const Contacts = () => {
@@ -17,36 +18,36 @@ export const Contacts = () => {
 
                         <ul className={s.myContact}>
                             <li>
-                                <div>Phone</div>
-                                <div><span className={s.icon}></span> +375296893021</div>
+                                <div className={s.text}>Phone</div>
+                                <div className={s.imgValueCotainer}><img className={s.icon} src={myContactsIcon.phone} alt=""/> +375296893021</div>
                             </li>
                             <li>
-                                <div>Email</div>
-                                <div><span className={s.icon}></span> askerko50@gmail.com</div>
+                                <div className={s.text}>Email</div>
+                                <div className={s.imgValueCotainer}><img className={s.icon} src={myContactsIcon.email} alt=""/> askerko50@gmail.com</div>
                             </li>
                             <li>
-                                <div>Github</div>
-                                <div><span className={s.icon}></span>Bmwbyas</div>
+                                <div className={s.text}>Github</div>
+                                <div className={s.imgValueCotainer}> <img className={s.icon} src={myContactsIcon.github} alt=""/>Bmwbyas</div>
                             </li>
                             <li>
-                                <div>Phone</div>
-                                <div><span className={s.icon}></span> +375 29 689 30 21</div>
+                                <div className={s.text}>Telegram</div>
+                                <div className={s.imgValueCotainer}><img className={s.icon} src={myContactsIcon.telegram} alt=""/> @askerkoSasha</div>
                             </li>
                         </ul>
 
 
                     <div className={s.formContainer}>
 
-                        <div>
+                        <p>
                             If you have any suggestion, project or even you want to say
                             Hello.. please fill out the form below and I will reply you shortly.
-                        </div>
+                        </p>
                         <form
                             action="https://public.herotofu.com/v1/2f662b40-7f8b-11ed-b38f-a1ed22f366b1"
                             method="post"
                             className={s.form}
                         >
-                            <input name="Name" id="name" placeholder="Your name" required/>
+                            <input className={s.name} name="Name" id="name" placeholder="Your name" required/>
                             <input name="Email" id="email" type="email" placeholder="Your email" required/>
                             <textarea name="Message" id="message" placeholder="Your message here" required></textarea>
                             <Button type={"submit"} name={'send message'}/>
--- src/components/Contacts/myContacts.ts
+++ src/components/Contacts/myContacts.ts
@@ -1,28 +1,17 @@
-export type myContactsType={
-    title:string
-    value:string
-    img:string
-}
+import user from './../../assets/image/socialIcon/icons8-user.png'
+import phone from './../../assets/image/socialIcon/icons8-whatsapp.svg'
+import email from './../../assets/image/socialIcon/icons8-message.png'
+import telegram from './../../assets/image/socialIcon/icons8-telegram.svg'
+import github from './../../assets/image/socialIcon/icons8-github.svg'
+import chat from './../../assets/image/socialIcon/icons8-chat.png'
+import message from './../../assets/image/socialIcon/icons8-messageFilled.png'
 
-export const myContacts:myContactsType[]=[
-    {
-        title:'phone',
-        value:'+375 29 689 30 21',
-        img:'dd'
-    },
-    {
-        title:'email',
-        value:'askerko50@gmail.com',
-        img:'dd'
-    },
-    {
-        title:'github',
-        value:'Bmwbyas',
-        img:'dd'
-    },
-    {
-        title:'telegram',
-        value:'@askerkoSasha',
-        img:'dd'
-    },
-]
+export const myContactsIcon={
+    user,
+    phone,
+    email,
+    telegram,
+    github,
+    chat,
+    message,
+}
