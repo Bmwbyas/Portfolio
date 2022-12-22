c960513f8083be6efe1748f2e8d24486f4a7684c
Askerko Alexandr <askerko50@gmail.com>
2022-12-22 20:47:32 +0300

    fix(my project fix button and description)

--- src/components/MyProjects/Project/Project.tsx
+++ src/components/MyProjects/Project/Project.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import s from './Project.module.scss'
 import {Button} from "../../../common/components/Button/Button";
-type WorKPropsType={
-    nameProject:string
-    description:string
-    imageIcon:{
-        backgroundImage:string
-        backgroundSize:string
-        backgroundPosition:string
+
+type WorKPropsType = {
+    nameProject: string
+    description: string
+    imageIcon: {
+        backgroundImage: string
+        backgroundSize: string
+        backgroundPosition: string
     }
 
 }
-export const Project = (props:WorKPropsType) => {
+export const Project = (props: WorKPropsType) => {
     return (
         <div className={s.workContainer}>
             <div className={s.imgAndButtonContainer} style={props.imageIcon}>
                 <div className={s.descriptionContainer}>
                     <h3 className={s.titleProject}>{props.nameProject}</h3>
                     <p className={s.descriptionProject}>{props.description}</p>
-                    <Button name={'view project'}/>
-
-                    <Button name={'view repository'}/>
+                    <div className={s.buttonContainer}>
+                        <Button name={'view project'}/>
+                        <Button name={'view repository'}/>
+                    </div>
                 </div>
+
 
             </div>
 
